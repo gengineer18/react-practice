424f7d3c4324b875f64c432dda5ebcc48761813d
妹尾弦 <gengineer.web@gmail.com>
2020-05-20 23:58:18 +0900

    learn about component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => {console.log('clicked!')}}/>
-      </>
-    )
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => {console.log('clicked!')}}/>
+//       </>
+//     )
+//   }
+// }
+
+const App = () => {
+  return (
+    <>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
