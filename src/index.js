e099fe65687243f6638655f9debd0e68cfc62478
妹尾弦 <gengineer.web@gmail.com>
2020-05-25 08:55:15 +0900

    イベント一覧の出力

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import './index.css';
+import thunk from 'redux-thunk'
+import './index.css'
 import reducer from './reducers'
-import EventsIndex from './components/events_index';
-import * as serviceWorker from './serviceWorker';
+import EventsIndex from './components/events_index'
+import * as serviceWorker from './serviceWorker'
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
@@ -19,4 +20,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
