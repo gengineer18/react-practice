cdde77b24c3cf0005073a15a097f30d43c49a6e2
妹尾弦 <gengineer.web@gmail.com>
2020-05-26 23:15:21 +0900

    Create機能の追加

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export const READ_EVENTS = 'READ_EVENTS'
+export const CREATE_EVENTS = 'CREATE_EVENTS'
 
 // thunkを使うことで、関数を返すことができる
 // export const readEvents = () => ({
@@ -13,4 +14,9 @@ const QUERYSTRING = '?token=token123'
 export const readEvents = () => async dispatch => {
   const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
   dispatch({ type: READ_EVENTS, response })
+}
+
+export const postEvent = values => async dispatch => {
+  const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`, values)
+  dispatch({ type: CREATE_EVENTS, response })
 }
