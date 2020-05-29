ccd401a64efc074ee4acc0f763b6ec1591ec11a8
妹尾弦 <gengineer.web@gmail.com>
2020-05-29 21:47:01 +0900

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,7 @@ export const READ_EVENTS = 'READ_EVENTS'
 export const CREATE_EVENT = 'CREATE_EVENT'
 export const READ_EVENT = 'READ_EVENT'
 export const DELETE_EVENT = 'DELETE_EVENT'
+export const UPDATE_EVENT = 'UPDATE_EVENT'
 
 // thunkを使うことで、関数を返すことができる
 // export const readEvents = () => ({
@@ -31,4 +32,9 @@ export const getEvent = id => async dispatch => {
 export const deleteEvent = id => async dispatch => {
   await axios.delete(`${ROOT_URL}/events/${id}${QUERYSTRING}`)
   dispatch({ type: DELETE_EVENT, id })
+}
+
+export const putEvent = values => async dispatch => {
+  const response = await axios.put(`${ROOT_URL}/events/${values.id}${QUERYSTRING}`, values)
+  dispatch({ type: UPDATE_EVENT, response })
 }
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -2,12 +2,16 @@ import _ from 'lodash'
 import {
   READ_EVENT,
   READ_EVENTS,
-  DELETE_EVENT
+  DELETE_EVENT,
+  UPDATE_EVENT,
+  CREATE_EVENT
 } from '../actions'
 
 export default (events = {}, action) => {
   switch (action.type) {
     case READ_EVENT:
+    case UPDATE_EVENT:
+    case CREATE_EVENT:
       const data = action.response.data
       return { ...events, [data.id]: data }
     case READ_EVENTS:
