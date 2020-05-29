3c2ed15b00da5e7c0d367e030fd3c4c660c1e524
妹尾弦 <gengineer.web@gmail.com>
2020-05-29 15:57:48 +0900

    mplement delete feature

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import './index.css'
 import reducer from './reducers'
 import EventsIndex from './components/events_index'
 import EventsNew from './components/events_new'
+import EventsShow from './components/events_show'
 import * as serviceWorker from './serviceWorker'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { composeWithDevTools } from 'redux-devtools-extension';
@@ -20,8 +21,10 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <Switch>
-        <Route exact path="/events/new" component={EventsNew}></Route>
+        <Route path="/events/new" component={EventsNew}></Route>
+        <Route path="/events/:id" component={EventsShow}></Route>
         <Route exact path="/" component={EventsIndex}></Route>
+        <Route exact path="/events" component={EventsIndex}></Route>
       </Switch>
     </BrowserRouter>
   </Provider>,
